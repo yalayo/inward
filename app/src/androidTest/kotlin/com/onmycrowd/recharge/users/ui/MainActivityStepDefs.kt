3b26d644f0db019817531006adb2d8584373cdc3
Mylab <labs@onmycrowd.com>
2020-02-25 16:37:07 -0700

    Creating new class for main activity steps definitions.

---
+++ app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
@@ -0,0 +1,29 @@
+package com.onmycrowd.recharge.users.ui
+
+import cucumber.api.PendingException
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+
+class MainActivityStepDefs {
+    @Given("the app is opened by a not authenticated user")
+    fun the_app_is_opened_by_a_not_authenticated_user() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @When("the main activity is shown")
+    fun the_main_activity_is_shown() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @Then("there should be a link to  sing in")
+    fun there_should_be_a_link_to_sing_in() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+
+    @Then("there should be a link to sign up")
+    fun there_should_be_a_link_to_sign_up() { // Write code here that turns the phrase above into concrete actions
+        throw PendingException()
+    }
+}
