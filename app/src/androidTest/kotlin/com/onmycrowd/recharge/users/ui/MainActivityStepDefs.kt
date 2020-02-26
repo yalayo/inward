2f2ecf0549d129cb04b59f5815fdaf7ada2edc96
Mylab <labs@onmycrowd.com>
2020-02-25 18:07:42 -0700

    Initial implementation of steps definitions.

--- app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
+++ app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
@@ -1,5 +1,12 @@
 package com.onmycrowd.recharge.users.ui
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.onmycrowd.recharge.R
+import com.onmycrowd.recharge.main.ui.MainActivity
 import cucumber.api.PendingException
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
@@ -7,23 +14,26 @@ import cucumber.api.java.en.When
 
 
 class MainActivityStepDefs {
+    private val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
     @Given("the app is opened by a not authenticated user")
-    fun the_app_is_opened_by_a_not_authenticated_user() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun the_app_is_opened_by_a_not_authenticated_user() {
+        ActivityScenario.launch(MainActivity::class.java)
     }
 
     @When("the main activity is shown")
-    fun the_main_activity_is_shown() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun the_main_activity_is_shown() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.main_activity)).check(matches(isDisplayed()))
     }
 
     @Then("there should be a link to  sing in")
-    fun there_should_be_a_link_to_sing_in() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun there_should_be_a_link_to_sing_in() {
+        ActivityScenario.launch(MainActivity::class.java)
     }
 
     @Then("there should be a link to sign up")
-    fun there_should_be_a_link_to_sign_up() { // Write code here that turns the phrase above into concrete actions
-        throw PendingException()
+    fun there_should_be_a_link_to_sign_up() {
+        ActivityScenario.launch(MainActivity::class.java)
     }
 }
