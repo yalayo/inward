fd5d77b7efd3bb781a447978f5d6a9b94649cdbf
Mylab <labs@onmycrowd.com>
2020-03-08 10:21:35 -0700

    Refactoring steps definition and adding to new steps.

--- app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
+++ app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
@@ -1,8 +1,12 @@
 package com.onmycrowd.recharge.users.ui
 
+import android.content.Intent
+import android.provider.ContactsContract
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.core.internal.deps.guava.collect.Iterables
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.onmycrowd.recharge.R
 import com.onmycrowd.recharge.main.ui.MainActivity
@@ -17,24 +21,32 @@ class MainActivityStepDefs {
 
     @Given("the app is opened by a not authenticated user")
     fun the_app_is_opened_by_a_not_authenticated_user() {
-        ActivityScenario.launch(MainActivity::class.java)
     }
 
     @When("the main activity is shown")
     fun the_main_activity_is_shown() {
-        ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main_activity)).check(matches(isDisplayed()))
     }
 
     @Then("there should be a link to  sing in")
     fun there_should_be_a_link_to_sing_in() {
-        ActivityScenario.launch(MainActivity::class.java)
         onView(withText("Sign In")).check(matches(isDisplayed()))
     }
 
     @Then("there should be a link to sign up")
     fun there_should_be_a_link_to_sign_up() {
-        ActivityScenario.launch(MainActivity::class.java)
         onView(withText("Sign Up")).check(matches(isDisplayed()))
+    }
+
+    @Then("the sign in link is tapped")
+    fun the_sign_in_link_is_tapped() {
+        onView(withId(R.id.signin)).perform(click())
+
+        //val receivedIntent = Iterables.getOnlyElement(Intents.getIntents());
+        //assertThat(receivedIntent).hasAction(Intent.ACTION_CALL)
+    }
+
+    @Then("the sign in activity should be shown")
+    fun the_sign_in_activity_should_be_shown() {
     }
 }
