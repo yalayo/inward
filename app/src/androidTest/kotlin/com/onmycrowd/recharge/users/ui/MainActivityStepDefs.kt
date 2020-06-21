43a3b41f871047074ce0383b5541f456a86a9190
Mylab <labs@onmycrowd.com>
2020-06-21 12:47:04 -0700

    Working in testing navigation between activities.

--- app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
+++ app/src/androidTest/kotlin/com/onmycrowd/recharge/users/ui/MainActivityStepDefs.kt
@@ -1,5 +1,7 @@
 package com.onmycrowd.recharge.users.ui
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
 import android.provider.ContactsContract
 import androidx.test.core.app.ActivityScenario
@@ -7,6 +9,10 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.core.internal.deps.guava.collect.Iterables
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.onmycrowd.recharge.R
 import com.onmycrowd.recharge.main.ui.MainActivity
@@ -14,10 +20,15 @@ import cucumber.api.PendingException
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
+import org.hamcrest.core.AllOf.allOf
+import org.junit.Rule
 
 
 class MainActivityStepDefs {
     private val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Given("the app is opened by a not authenticated user")
     fun the_app_is_opened_by_a_not_authenticated_user() {
@@ -40,7 +51,12 @@ class MainActivityStepDefs {
 
     @Then("the sign in link is tapped")
     fun the_sign_in_link_is_tapped() {
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, Intent())
+        intending(hasAction(Intent.ACTION_CHOOSER)).respondWith(result);
+
         onView(withId(R.id.signin)).perform(click())
+
+        intended(allOf(hasAction(Intent.ACTION_CALL)));
 
         //val receivedIntent = Iterables.getOnlyElement(Intents.getIntents());
         //assertThat(receivedIntent).hasAction(Intent.ACTION_CALL)
